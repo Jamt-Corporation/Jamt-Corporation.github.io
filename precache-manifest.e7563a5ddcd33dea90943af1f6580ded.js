73fcad88330365e015fa2121a96944f166d9ff74
jamt0 <64938727+jamt0@users.noreply.github.com>
2020-05-12 00:11:16 -0300

    Sube pagina
    Se supena la pagina con el build hecho, primer buils

---
+++ precache-manifest.e7563a5ddcd33dea90943af1f6580ded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5d20f3808e73e0004034e0958abbce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b71ea0cae2f383c9a70",
+    "url": "/static/css/main.457e73bb.chunk.css"
+  },
+  {
+    "revision": "379227aa00fe9883bbc9",
+    "url": "/static/js/2.5200b7af.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/static/js/2.5200b7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b71ea0cae2f383c9a70",
+    "url": "/static/js/main.09c21ffc.chunk.js"
+  },
+  {
+    "revision": "3269e9a732fe4a5519a3",
+    "url": "/static/js/runtime-main.a0d94967.js"
+  },
+  {
+    "revision": "cc6d49546afef616415337d46c9abd95",
+    "url": "/static/media/Mermelada.cc6d4954.jpg"
+  }
+]);
