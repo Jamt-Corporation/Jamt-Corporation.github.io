a8a1eeca7a0a30c04eaa28695107f1c0c9665f40
jamt0 <64938727+jamt0@users.noreply.github.com>
2020-05-15 17:31:07 -0300

    intento 9

---
+++ precache-manifest.d5846a2c0f86161f4040f9bde2dd93ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ace69b7927e7196790de2730231a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2966103ce68a62a0b44c",
+    "url": "/static/css/main.dbf7f038.chunk.css"
+  },
+  {
+    "revision": "379227aa00fe9883bbc9",
+    "url": "/static/js/2.5200b7af.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/static/js/2.5200b7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2966103ce68a62a0b44c",
+    "url": "/static/js/main.f2841705.chunk.js"
+  },
+  {
+    "revision": "3269e9a732fe4a5519a3",
+    "url": "/static/js/runtime-main.a0d94967.js"
+  },
+  {
+    "revision": "91ae715c55a9d8bc5c1a30e80d5c38c1",
+    "url": "/static/media/MermeladaFondo.91ae715c.png"
+  }
+]);
